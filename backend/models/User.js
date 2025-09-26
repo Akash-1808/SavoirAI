6f83e4da0d44634901fffe8f4ed84cc847ed8326
Raman Singh <rsingh24595@gmail.com>
2025-09-26 13:26:45 +0530

    backend

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,9 +6,17 @@ const educationSchema = new mongoose.Schema({
   year: { type: Number, required: true },        // passing year
   percentage: { type: Number }                   // optional
 });
+const pastJobExperience = new mongoose.Schema({
+  companyName: {type:String, required:true},
+  role: {type:String, required:true},
+  startDate: {type:Date, required:true},
+  endDate: {type:Date, required:true},
+  description : {type:String, required:false}
+});
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true, trim: true },
+  firstName: { type: String, required: true, trim: true },
+  lastName: { type: String, required: true, trim: true },
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true, minlength: 6 },
   role: { type: String, enum: ["candidate", "admin", "employer"], default: "candidate" },
@@ -16,6 +24,7 @@ const userSchema = new mongoose.Schema({
   interests: { type: [String], default: [] },    // array of strings
   education: { type: [educationSchema], default: [] }, // multiple education levels
   location: { type: String, required: true },
+  experience: {type: [pastJobExperience], default: []},
   appliedInternships: [{ type: mongoose.Schema.Types.ObjectId, ref: "Internship" }],
   createdAt: { type: Date, default: Date.now }
 });
